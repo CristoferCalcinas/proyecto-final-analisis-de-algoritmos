05e0275d98776def6541d8fdec6f5c3ed9201a28
adrian <adrian@google.com>
2024-06-05 21:28:28 -0400

    agregando lista de estudiantes

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,8 +1,10 @@
+import { StudentList } from "@/components";
 
 export default function AccordionPage() {
     return (
         <div>
             <h1>Hello Page</h1>
+            <StudentList />
         </div>
     );
 }
