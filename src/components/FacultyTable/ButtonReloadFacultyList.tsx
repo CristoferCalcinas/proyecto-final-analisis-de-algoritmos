9f263e89ac644ff0dbf62305fb1d12b684d35f91
adrian <adrian@google.com>
2024-06-11 10:41:50 -0400

    feat: Agregar funcionalidad de recarga de tablas en la lista de facultades

--- src/components/FacultyTable/ButtonReloadFacultyList.tsx
+++ src/components/FacultyTable/ButtonReloadFacultyList.tsx
@@ -1,9 +1,18 @@
+import { UpdateIcon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+
 interface Props {
     reloadTables: () => void;
 }
 
 export const ButtonReloadFacultyList = ({ reloadTables }: Props) => {
     return (
-        <div>ButtonReloadStudentsList</div>
+        <Button
+            variant='outline'
+            size="icon"
+            onClick={reloadTables}
+        >
+            <UpdateIcon className='h-4 w-4' />
+        </Button>
     )
 }
