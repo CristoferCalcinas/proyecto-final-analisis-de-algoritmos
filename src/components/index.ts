228450062d1231a71e912da2e38906825c032cf1
adrian <adrian@google.com>
2024-06-11 12:22:37 -0400

    feat: Agregar dependencias @headlessui/react y @heroicons/react ademas de agregar el componente StudnetSearch

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 
 
 export * from './FacultyTable/FacultyTable'
+export * from './FindStudent/StudentSearch'
 export * from './HomePage/Presentation'
 export * from './StudentList/StudentList'
 export * from './UserAvatar/UserAvatar'
