a5b3b446156c511faf96ef6357062b7ec71feab3
adrian <adrian@google.com>
2024-06-16 15:23:24 -0400

    feat: Agregar funciones para agregar, actualizar y eliminar facultades

--- src/actions/faculty-list-actions.ts
+++ src/actions/faculty-list-actions.ts
@@ -47,3 +47,79 @@ export const add_faculty = async (faculty: { nombre: string; descripcion: string
         };
     }
 }
+
+export const update_faculty = async (faculty: { id: number; nombre: string; descripcion: string; }) => {
+    try {
+        await prisma.$queryRaw`
+            Call actualizar_facultad(
+                    ${faculty.id},
+                    ${faculty.nombre},
+                    ${faculty.descripcion}
+                )
+            `
+        return {
+            status: 200,
+            message: 'Facultad actualizada correctamente'
+        }
+    } catch (error) {
+        console.log('\n\n-------------------------ERROR-UPDATE-FACULTY--------------------------');
+        console.log(error);
+        console.log('\n\n-------------------------ERROR-UPDATE-FACULTY--------------------------');
+
+        if (error instanceof Error) {
+            return {
+                status: 500,
+                message: 'Error Desconocido'
+            }
+        };
+
+        if (error instanceof PrismaClientKnownRequestError) {
+            const errorMessage = error.meta as { message: string };
+
+            return {
+                error: errorMessage?.message,
+                status: 400
+            };
+        };
+
+        return {
+            status: 500,
+            message: 'Error desconocido'
+        };
+    }
+}
+
+export const delete_faculty = async (faculty: { id: number; }) => {
+    try {
+        await prisma.facultad.deleteMany({
+            where: {
+                id: faculty.id
+            }
+        });
+    } catch (error) {
+        console.log('\n\n-------------------------ERROR-DELETE-FACULTY--------------------------');
+        console.log(error);
+        console.log('\n\n-------------------------ERROR-DELETE-FACULTY--------------------------');
+
+        if (error instanceof Error) {
+            return {
+                status: 500,
+                message: 'Error Desconocido'
+            }
+        };
+
+        if (error instanceof PrismaClientKnownRequestError) {
+            const errorMessage = error.meta as { message: string };
+
+            return {
+                error: errorMessage?.message,
+                status: 400
+            };
+        };
+
+        return {
+            status: 500,
+            message: 'Error desconocido'
+        };
+    }
+}
